f14d88c435d8e9c7dcfca197eddeaee05844268f
sahn111 <you@example.com>
2022-12-02 11:37:52 +0300

    warning update

--- Models/LoginDTO.cs
+++ Models/LoginDTO.cs
@@ -2,7 +2,7 @@ namespace ToDoApi.Model
 {
     public class LoginDTO
     {
-        public string UserName { get; set; }
-        public string Password { get; set; }
+        public string? UserName { get; set; }
+        public string? Password { get; set; }
     }
 }
